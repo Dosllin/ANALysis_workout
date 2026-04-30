c604fe2498900dc9e71d2880f3ef61221e15c993
Никита Прохоров <89354375+Dosllin@users.noreply.github.com>
2026-05-01 01:06:31 +0300

    В декараторах лежал lad data почему то

--- src/decorators.py
+++ src/decorators.py
@@ -1,88 +1,47 @@
 from datetime import datetime
 import os
-import pandas as pd
+from functools import wraps
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+SESSION_TIME = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+LOG_FILE_PATH = f'logs/session_{SESSION_TIME}.txt'
 
 
-def check_data():
-    if not os.path.exists('workout_data.csv'):
-        print("Нужно создать базу тренеровок!")
-        df = pd.DataFrame(
-            columns=['Date', 'Workout_Split', 'Exercise', 'Sets', 'Reps', 'Weight_kg', 'RPE', 'Duration_min'])
-        create_data(df)
-
-
-def create_data(df):
-    # df = pd.read_csv('workout_data.csv')
-    print("=== Режим добавления тренировок ===")
-
-    while True:
-        user_data = input("\nВведите дату тренировки в формате ГГГГ-ММ-ДД или stop если хотите остановиться: ")
-
-        if user_data.lower() == 'stop':
-            if len(df) == 0:
-                print("В базе пока нет данных, введите хотя бы 1 тренировку!")
-                continue
-            else:
-                break
-
+def log_action(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        action_time = datetime.now().strftime('%H:%M:%S')
+        with open(LOG_FILE_PATH, 'a', encoding='utf-8') as log_file:
+            log_file.write(f"[{action_time}] ЗАПУСК функции: {func.__name__}\n")
+            if len(args) > 1 or kwargs:
+                log_file.write(f"    Параметры поиска: args={args[1:]}, kwargs={kwargs}\n")  # [1:] убираем self
         try:
-            date = datetime.strptime(user_data, '%Y-%m-%d')
-        except ValueError:
-            print('Некоретный ввод даты, пример даты: 2021-12-31')
-            continue
-        workout_split = input("Введите тип тренировки (Chest/Triceps, Back/Biceps, Legs/Shoulders): ")
-        exercise = input("Введите конкретное упражнение (Deadlift, Pull-up, Squat и т.д.): ")
-        while True:
-            try:
-                sets = int(input("Введите количество подходов: "))
-                reps = int(input("Введите количество повторений в подходе: "))
-                break
-            except ValueError:
-                print('Введите целое число!!!')
+            result = func(*args, **kwargs)
+            with open(LOG_FILE_PATH, 'a', encoding='utf-8') as log_file:
+                log_file.write(f"[{datetime.now().strftime('%H:%M:%S')}] УСПЕХ: {func.__name__} выполнен без ошибок\n")
+            return result
 
-        while True:
-            try:
-                weight_kg = float(input("Введите рабочий вес (kg): "))
-                break
-            except ValueError:
-                print('Введите число (Можно не целое, например 40.1)!!!!')
+        except Exception as e:
+            with open(LOG_FILE_PATH, 'a', encoding='utf-8') as log_file:
+                log_file.write(f"[{datetime.now().strftime('%H:%M:%S')}] ОШИБКА в {func.__name__}: {e}\n")
+            raise e
 
-        while True:
-            try:
-                rpe = int(input("Введите шкала тяжести от 1 до 10: "))
-                if 1 <= rpe <= 10:
-                    break
-                else:
-                    print('Введите число от 1-10!!!!')
-            except ValueError:
-                print('Введите целое число!!!')
-
-        while True:
-            try:
-                duration_min = int(input("Введите общая длительность тренировки (min): "))
-                break
-            except ValueError:
-                print("Введите целое число!")
-
-        new_row = {
-            'Date': date,
-            'Workout_Split': workout_split,
-            'Exercise': exercise,
-            'Sets': sets,
-            'Reps': reps,
-            'Weight_kg': weight_kg,
-            'RPE': rpe,
-            'Duration_min': duration_min
-        }
-
-        df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-        print("Тренировка успешно добавлена!")
-
-    df.to_csv('workout_data.csv', index=False)
-    print(f"\n💾 Данные успешно сохранены в 'workout_data.csv'. Всего записей: {len(df)}")
+    return wrapper
 
 
-def load_data(check_y_or_n=True):
-    if check_y_or_n:
-        check_data()
-    return pd.read_csv('workout_data.csv')
+def format_output(title, on_disp=True):
+    def decorator_format_output(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(f"\n{'=' * 10} {title.upper()} {'=' * 10}")
+            result = func(*args, **kwargs)
+            if on_disp:
+                print(result)
+            print("=" * (22 + len(title)))
+            return result
+
+        return wrapper
+
+    return decorator_format_output
